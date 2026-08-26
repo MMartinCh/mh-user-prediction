89f479d1e3229c031f12bb4aec086b34d0c97b4f
MMartinCh <Moritz.Martin.c@gmail.com>
2026-08-27 01:22:42 +0200

    QuestItem expended. TransformedQuestItem added.

--- src/core/dataclasses/quest_data.py
+++ src/core/dataclasses/quest_data.py
@@ -3,7 +3,8 @@ from typing import Dict, List, Optional
 
 @dataclass
 class QuestItem:
-    """Item representing quest data for one quest."""
+    """Object representing quest data for one quest."""
+
     title: str
     quest_id: Optional[str] = None 
 
@@ -13,7 +14,11 @@ class QuestItem:
     rank: Optional[str] = None
     level: Optional[int] = None
 
+    hub: Optional[str] = None
+    location: Optional[str] = None
+
     is_assignment: Optional[bool] = False
+    is_key: Optional[bool] = False
     is_event: Optional[bool] = False
 
     targets: List[str] = field(default_factory=list)
@@ -21,3 +26,35 @@ class QuestItem:
 
     reward_zenny: Optional[int] = 0
     reward_points: Optional[int] = 0
+
+    requirement: Optional[int] = 0
+
+@dataclass
+class TransformedQuestItem:
+    """Object representing quest data as a monster attribute."""
+
+    title: str
+    quest_id: Optional[str] = None 
+
+    game: Optional[str] = None
+    generation: Optional[int] = None
+
+    rank: Optional[str] = None
+    level: Optional[int] = None
+
+    hub: Optional[str] = None
+    location: Optional[str] = None
+
+    is_assignment: Optional[bool] = False
+    is_key: Optional[bool] = False
+    is_event: Optional[bool] = False
+
+    hp: Optional[int] = None
+
+    reward_zenny: Optional[int] = 0
+    reward_points: Optional[int] = 0
+
+    requirement: Optional[int] = 0
+
+
+
---
+++ src/core/transformers/__init__.py
@@ -0,0 +1,3 @@
+from .cross_game_normalizer import CrossGameNormalizer
+
+__all__ = ["CrossGameNormalizer"]
